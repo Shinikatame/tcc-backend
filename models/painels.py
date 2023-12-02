de7c8deb47fe3263bfb2410f32600b1432009116
Hudson <hudson.farias.dev@gmail.com>
2023-12-02 11:27:16 -0300

    financial

--- models/painels.py
+++ models/painels.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
 
+
 class DashboardResponse(BaseModel):
     students: int
     scholarship_holder: int
     supports: int
+
+
+class FinancialResponse(BaseModel):
+    students_paying: int
+    students_scholarship: int
--- routers/admin/painels.py
+++ routers/admin/painels.py
@@ -3,7 +3,7 @@ from routers.admin import router
 
 from typing import List
 
-from models.dashboard import DashboardResponse
+from models.painels import DashboardResponse, FinancialResponse
 from database.user import UserORM
 from database.support import SupportORM
 
@@ -14,4 +14,15 @@ async def dashboard():
     supports = await SupportORM.find_many()
 
     response = DashboardResponse(students = len(students), scholarship_holder = len(scholarship_holder), supports = len(supports))
+    return response
+
+
+@router.get("/financial", response_model = FinancialResponse)
+async def financial():
+    students = await UserORM.find_many()
+
+    students_paying = [s for s in students if not s.scholarship_holder]
+    students_scholarship = [s for s in students if s.scholarship_holder]
+
+    response = FinancialResponse(students_paying = len(students_paying), students_scholarship = len(students_scholarship))
     return response
